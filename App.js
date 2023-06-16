25df82a43f7c57b74837b11665ff6aff90067800
Gael16062003 <ambeugael128@gmail.com>
2023-06-16 03:53:41 +0200

    create pages and routes

---
+++ App.js
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+import Navigation from "./src/components/Navigation";
+
+export default function App() {
+  return (
+    <Navigation />
+  );
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from "../pages/HomeScreen";
+import LoginScreen from "../pages/LoginScreen";
+import RegisterScreen from "../pages/RegisterScreen";
+
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () =>{
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="LoginScreen" component={LoginScreen} />
+                <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Navigation;
